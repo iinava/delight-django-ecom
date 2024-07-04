3afa715b915068cc493be96b5451d7a59dccbbfd
iinava <131179814+iinava@users.noreply.github.com>
2024-07-04 23:05:33 +0530

    feat(ui):product_details page added

--- product/urls.py
+++ product/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import (Home)
+from .views import (Home,ProductDetails)
 
 
 urlpatterns = [
-    path("", Home.as_view(), name="home")
+    path("", Home.as_view(), name="home"),
+    path("product_details/", ProductDetails.as_view(), name="product_details")
     
 ]
--- product/views.py
+++ product/views.py
@@ -5,3 +5,7 @@ from django.views import generic
 
 class Home(generic.TemplateView):
     template_name = 'home.html'
+    
+    
+class ProductDetails(generic.TemplateView):
+    template_name = 'product/product_details.html'
