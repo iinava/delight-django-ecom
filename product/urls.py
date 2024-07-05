f1697d90480fc682461475618a473be24487ea09
iinava <131179814+iinava@users.noreply.github.com>
2024-07-05 21:46:15 +0530

    feat(ui,view):category,details page added

--- product/urls.py
+++ product/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import (Home,ProductDetails)
+from .views import (Home,ProductDetails,CategoryDetails)
 
 
 urlpatterns = [
     path("", Home.as_view(), name="home"),
-    path("product_details/", ProductDetails.as_view(), name="product_details")
+    path("product_details/<str:slug>/", ProductDetails.as_view(), name="product_details"),
+    path("category_details/<str:slug>/", CategoryDetails.as_view(), name="category_details")
     
 ]
--- product/views.py
+++ product/views.py
@@ -1,14 +1,12 @@
 from django.shortcuts import render
 from django.views import generic
-from typing import Any, Dict  # Import Any and Dict from typing
-from .models import Category, Product, Slider  
-
-# Create your views here.
+from typing import Any, Dict
+from .models import Category, Product, Slider
 
 class Home(generic.TemplateView):
     template_name = 'home.html'
-    
-    def get_context_data(self, **kwargs) -> Dict[str, Any]:  # Use Dict from typing
+
+    def get_context_data(self, **kwargs) -> Dict[str, Any]:
         context = super().get_context_data(**kwargs)
         context.update({
             'featured_categories': Category.objects.filter(featured=True),
@@ -16,7 +14,23 @@ class Home(generic.TemplateView):
             'sliders': Slider.objects.filter(show=True),
         })
         return context
-    
-    
-class ProductDetails(generic.TemplateView):
+
+class ProductDetails(generic.DetailView):
+    model = Product
+    slug_url_kwarg = 'slug'
     template_name = 'product/product_details.html'
+
+    def get_context_data(self, **kwargs) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['related_products'] = self.get_object().get_related_products()
+        return context
+    
+class CategoryDetails(generic.DetailView):
+    model = Category
+    slug_url_kwarg = 'slug'
+    template_name = 'product/category_details.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['products'] = self.get_object().products.all()
+        return context
