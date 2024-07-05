d93ee299ad4af26adc42349c2a19692a3d9f8918
iinava <131179814+iinava@users.noreply.github.com>
2024-07-05 12:54:20 +0530

    feat(ui):home view implemented

--- product/views.py
+++ product/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.views import generic
-# Create your views here.
+from typing import Any, Dict  # Import Any and Dict from typing
+from .models import Category, Product, Slider  
 
+# Create your views here.
 
 class Home(generic.TemplateView):
     template_name = 'home.html'
+    
+    def get_context_data(self, **kwargs) -> Dict[str, Any]:  # Use Dict from typing
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'featured_categories': Category.objects.filter(featured=True),
+            'featured_products': Product.objects.filter(featured=True),
+            'sliders': Slider.objects.filter(show=True),
+        })
+        return context
     
     
 class ProductDetails(generic.TemplateView):
