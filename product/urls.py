251f9f8acec66c16c5491c07d4dc83c9aaac6234
iinava <131179814+iinava@users.noreply.github.com>
2024-07-06 13:33:05 +0530

    feat(ui,data):product list page, pagination

--- product/urls.py
+++ product/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import (Home,ProductDetails,CategoryDetails)
+from .views import (Home,ProductDetails,CategoryDetails,ProductList)
 
 
 urlpatterns = [
     path("", Home.as_view(), name="home"),
     path("product_details/<str:slug>/", ProductDetails.as_view(), name="product_details"),
-    path("category_details/<str:slug>/", CategoryDetails.as_view(), name="category_details")
+    path("category_details/<str:slug>/", CategoryDetails.as_view(), name="category_details"),
+    path("product_list/", ProductList.as_view(), name="product_list"),
+    
     
 ]
--- product/views.py
+++ product/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 from django.views import generic
 from typing import Any, Dict
 from .models import Category, Product, Slider
+from django.core.paginator import (
+    Paginator,
+    EmptyPage,
+    PageNotAnInteger,
+    InvalidPage
+)
+
 
 class Home(generic.TemplateView):
     template_name = 'home.html'
@@ -13,6 +20,7 @@ class Home(generic.TemplateView):
             'featured_products': Product.objects.filter(featured=True),
             'sliders': Slider.objects.filter(show=True),
         })
+        print(context)
         return context
 
 class ProductDetails(generic.DetailView):
@@ -34,3 +42,44 @@ class CategoryDetails(generic.DetailView):
         context = super().get_context_data(**kwargs)
         context['products'] = self.get_object().products.all()
         return context
+
+class CustomPagination:
+    def __init__(self, request, queryset, paginated_by):
+        self.paginated_by = paginated_by
+        self.paginator = Paginator(queryset, paginated_by)
+        self.queryset = queryset
+        self.page = request.GET.get('page', 1)
+        
+    def get_queryset(self):
+        try:
+            queryset = self.paginator.page(self.page)
+        except PageNotAnInteger:
+            queryset = self.paginator.page(1)
+        except EmptyPage:
+            queryset = self.paginator.page(1)
+        except InvalidPage:
+            queryset = self.paginator.page(1)
+        return queryset            
+            
+                         
+                           
+        
+
+ 
+class ProductList(generic.ListView):
+    model = Product
+    template_name = 'product/product_list.html'
+    context_object_name = 'object_list'
+    paginate_by = 8
+    
+    def  get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        page_obj = CustomPagination(self.request,self.get_queryset(),self.paginate_by)
+        paginator = page_obj.paginator
+        queryset = page_obj.get_queryset()
+        
+        context['object_list'] = queryset
+        context['paginator'] = paginator
+        return context
+        
+     
