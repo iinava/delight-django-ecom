4fe51c8c1ceca67d85dfc58dec7ad6476ed73afd
iinava <131179814+iinava@users.noreply.github.com>
2024-07-08 18:41:01 +0530

    fix:out of stock error handling

--- cart/views.py
+++ cart/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,get_object_or_404,redirect
 from .carts import Cart
 from django.views import generic
 from product.models import Product
+from django.contrib import messages
 
 # Create your views here.
 class AddToCart(generic.View):
@@ -23,12 +24,24 @@ class  CartItems(generic.TemplateView):
         clear=request.GET.get('clear',False)
         cart = Cart(request)
         if product_id and quantity:
-            cart = Cart(request)
-            cart.update(int(product_id),int(quantity))
-            return redirect('cart_items')
+            product = get_object_or_404(Product,id=product_id )
+            if int(quantity) > 0:   
+                if product.instock:
+                    cart = Cart(request)
+                    cart.update(int(product_id),int(quantity))
+                    return redirect('cart_items')
+                else:
+                    messages.warning(request,"the product is not in stock anymore")
+                    return redirect('cart_items')
+            else:
+                cart.update(int(product_id),int(quantity))
+                return redirect('cart_items')
+        
+        
         if clear:
             cart.clear()
             return redirect('cart_items')
+        
             
             
         return super().get(request,*args, **kwargs)
